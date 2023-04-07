d0e02c29d5cae0374a5bbfcf2e9dd0ea1db9829a
dharmik <dharmikbhingradiya2001@gmail.com>
2023-04-07 23:42:17 +0530

    #1 first commit to processor & added processor tests.

--- src/main/java/com/serviceops/assetdiscovery/rest/PhysicalDiskRest.java
+++ src/main/java/com/serviceops/assetdiscovery/rest/PhysicalDiskRest.java
@@ -2,7 +2,6 @@ package com.serviceops.assetdiscovery.rest;
 
 import java.io.Serializable;
 import java.sql.Date;
-import java.sql.Timestamp;
 import java.util.Objects;
 
 /**
---
+++ src/test/java/com/serviceops/assetdiscovery/mock/ProcessorTest.java
@@ -0,0 +1,4 @@
+package com.serviceops.assetdiscovery.mock;
+
+public class ProcessorTest {
+}
