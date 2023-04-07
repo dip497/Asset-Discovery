df3ac790596acc0301c5b532a875587b66eebefb
dipendra sharma <dipendra497@gmail.com>
2023-04-07 11:29:16 +0530

    #1 init

---
+++ src/main/java/com/serviceops/assetdiscovery/AssetdiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.serviceops.assetdiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetdiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssetdiscoveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/serviceops/assetdiscovery/AssetdiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serviceops.assetdiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetdiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
