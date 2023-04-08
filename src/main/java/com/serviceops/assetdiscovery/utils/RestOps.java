da035c2cc1ec89f194223019fef38faabe74a05f
dipendra sharma <dipendra497@gmail.com>
2023-04-08 12:12:02 +0530

    test push

---
+++ src/main/java/com/serviceops/assetdiscovery/utils/RestOps.java
@@ -0,0 +1,9 @@
+package com.serviceops.assetdiscovery.utils;
+
+import com.serviceops.assetdiscovery.entity.mapped.Base;
+import com.serviceops.assetdiscovery.entity.mapped.SingleBase;
+import com.serviceops.assetdiscovery.rest.SingleBaseRest;
+
+public class RestOps<T extends Base, R extends Base> {
+
+}
