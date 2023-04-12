e238d1be8cb7306a1ea626878fcf26d0ac121914
dharmik <dharmikbhingradiya2001@gmail.com>
2023-04-12 20:20:46 +0530

    pointing device.

--- src/main/java/com/serviceops/assetdiscovery/controller/PointingDeviceController.java
+++ src/main/java/com/serviceops/assetdiscovery/controller/PointingDeviceController.java
@@ -1,11 +1,15 @@
 package com.serviceops.assetdiscovery.controller;
 
+import com.serviceops.assetdiscovery.rest.PointingDeviceRest;
 import com.serviceops.assetdiscovery.service.interfaces.PointingDeviceService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+
+@RestController
+@RequestMapping("/{refId}/pointingDevices")
 public class PointingDeviceController {
     private final PointingDeviceService pointingDeviceService;
     private final Logger logger = LoggerFactory.getLogger(PointingDeviceController.class);
@@ -14,7 +18,23 @@ public class PointingDeviceController {
         this.pointingDeviceService = pointingDeviceService;
     }
 
-    public void addPointingDevice(){
+    @GetMapping
+    public List<PointingDeviceRest> getPointingDeviceList(@PathVariable("refId") String refId){
+        logger.debug("getPointing DeviceList with Asset id: -->{}",refId);
+        List<PointingDeviceRest> pointingDeviceList = pointingDeviceService.getPointingDevices(Long.valueOf(refId));
 
+        return pointingDeviceList;
+    }
+
+    @DeleteMapping
+    public void deletePointingDevice(@PathVariable("refId") String refId){
+        logger.debug("PointingDevice deleting with id --> {}",refId);
+        pointingDeviceService.deleteById(Long.parseLong(refId));
+    }
+
+    @PutMapping
+    public void updatePointingDevice(@PathVariable("refId") String refId, @RequestBody PointingDeviceRest pointingDeviceRest){
+        logger.debug("PointingDevice updating with id --> {}",refId);
+        pointingDeviceService.update(Long.parseLong(refId), pointingDeviceRest);
     }
 }
--- src/main/java/com/serviceops/assetdiscovery/service/interfaces/PointingDeviceService.java
+++ src/main/java/com/serviceops/assetdiscovery/service/interfaces/PointingDeviceService.java
@@ -1,8 +1,13 @@
 package com.serviceops.assetdiscovery.service.interfaces;
 
-import com.serviceops.assetdiscovery.entity.PointingDevice;
-import org.springframework.stereotype.Service;
+
+import com.serviceops.assetdiscovery.rest.PointingDeviceRest;
+
+import java.util.List;
 
 public interface PointingDeviceService {
     void  save(Long id);
+    void update(Long id, PointingDeviceRest pointingDeviceRest);
+    void deleteById(Long id);
+    List<PointingDeviceRest> getPointingDevices(Long id);
 }
