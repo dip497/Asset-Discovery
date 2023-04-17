29bf2b1628636e8d9c779434662326ed3e709e3a
dharmik <dharmikbhingradiya2001@gmail.com>
2023-04-17 15:34:44 +0530

    pointing device manufacturer added.

--- src/main/java/com/serviceops/assetdiscovery/controller/PointingDeviceController.java
+++ src/main/java/com/serviceops/assetdiscovery/controller/PointingDeviceController.java
@@ -4,12 +4,12 @@ import com.serviceops.assetdiscovery.rest.PointingDeviceRest;
 import com.serviceops.assetdiscovery.service.interfaces.PointingDeviceService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/{refId}/pointingDevices")
 public class PointingDeviceController {
     private final PointingDeviceService pointingDeviceService;
     private final Logger logger = LoggerFactory.getLogger(PointingDeviceController.class);
@@ -18,19 +18,20 @@ public class PointingDeviceController {
         this.pointingDeviceService = pointingDeviceService;
     }
 
-    @GetMapping()
+    @GetMapping(value = "/{refId}/pointingDevices",produces = MediaType.APPLICATION_JSON_VALUE)
     public List<PointingDeviceRest> getPointingDeviceList(@PathVariable("refId") Long refId){
         logger.debug("getPointing DeviceList with Asset id: -->{}",refId);
         return  pointingDeviceService.getPointingDevices(refId);
     }
 
-    @DeleteMapping()
+//    @DeleteMapping(value = "/{refId}/pointingDevices")
+    @DeleteMapping(value = "/{refId}/pointingDevices",consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     public void deletePointingDevice(@PathVariable("refId") Long refId){
         logger.debug("PointingDevice deleting with id --> {}",refId);
         pointingDeviceService.deleteById(refId);
     }
 
-    @PutMapping()
+    @PutMapping(value = "/{refId}/pointingDevices",consumes = MediaType.APPLICATION_JSON_VALUE)
     public void updatePointingDevice(@PathVariable("refId") String refId, @RequestBody PointingDeviceRest pointingDeviceRest){
         logger.debug("PointingDevice updating with id --> {}",refId);
         pointingDeviceService.update(Long.parseLong(refId), pointingDeviceRest);
